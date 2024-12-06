c01a43b242bfd5087c7c5b3924a42dcea6c6cb5b
zouzanyan <qq1406823510@163.com>
2024-12-06 10:43:58 +0800

    fix

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,7 +7,6 @@ public class Application {
     public static void main(String[] args) {
         KuzkasBootStrap kuzkasBootStrap = new KuzkasBootStrap();
         kuzkasBootStrap.KuzkasStart();
-        System.out.println("zouzanyan-feature测试");
     }
 
 }
